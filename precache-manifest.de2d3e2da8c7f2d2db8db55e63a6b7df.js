1f96a558684d4c99b6ea416f4bd936bec9751ad8
Mohemmed Faher Ali <fahershaheen@gmail.com>
2020-03-20 22:15:45 +0500

    Updates

---
+++ precache-manifest.de2d3e2da8c7f2d2db8db55e63a6b7df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff9f3ec8be8fe910eaaaa3c31d025c9",
+    "url": "/React-todo-sample/index.html"
+  },
+  {
+    "revision": "06b8da23849ec9baf5f9",
+    "url": "/React-todo-sample/static/css/main.0c84d867.chunk.css"
+  },
+  {
+    "revision": "d59b6ea96014882ff4bc",
+    "url": "/React-todo-sample/static/js/2.c16ec7aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo-sample/static/js/2.c16ec7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06b8da23849ec9baf5f9",
+    "url": "/React-todo-sample/static/js/main.08a8269e.chunk.js"
+  },
+  {
+    "revision": "5bcb7d7177d9d46019e1",
+    "url": "/React-todo-sample/static/js/runtime-main.04f15c6e.js"
+  }
+]);
